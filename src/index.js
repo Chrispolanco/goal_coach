48b0a5542c563d8efeedd11717df1f29fadb8796
Chrispolanco <chrispolanco@sbcglobal.net>
2021-12-31 17:15:09 -0800

    logic added to three components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'; 
 import ReactDom from 'react-dom'; 
+import App from './component/App'; 
+import SignIn from './component/SignIn'; 
+import SignUp from './component/SignUp'; 
 
 ReactDom.render(
     <div>App</div>, document.getElementById('root')
